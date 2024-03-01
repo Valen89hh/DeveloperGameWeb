1133f43b66ca89639a3cb2111173d59c72b86c65
Valen <126835108+Valen89hh@users.noreply.github.com>
2024-03-01 10:07:20 -0500

    add section about

--- src/components/general/Containers/Container.tsx
+++ src/components/general/Containers/Container.tsx
@@ -1,14 +1,16 @@
 interface ContainerProps{
     style?: string,
-    children: React.ReactNode
+    children: React.ReactNode,
+    id?: string
 }
 
 
 const Container: React.FC<ContainerProps> = ({
     style,
-    children
+    children,
+    id
 }) => {
-    return ( <div className={`container mx-auto ${style} px-2 sm:px-0`}>
+    return ( <div className={`container mx-auto ${style} px-2 sm:px-0`} id={id}>
         {children}
     </div> );
 }
