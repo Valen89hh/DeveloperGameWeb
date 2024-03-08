290a260cf2f22308cd0bc73440a8462650403c8f
Valen <126835108+Valen89hh@users.noreply.github.com>
2024-03-07 23:44:41 -0500

    correction send message

--- src/components/pages/home/Contact/ContactSection.tsx
+++ src/components/pages/home/Contact/ContactSection.tsx
@@ -3,7 +3,7 @@
 import ContainerCenter from "@/components/general/Containers/CotainerCenter";
 import { InputContact, TextFieldContact } from "./Inputs";
 import ButtonPrimary from "@/components/general/Buttons/ButtonPrimary";
-import { useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 const ContactSection = () => {
 
@@ -14,16 +14,44 @@ const ContactSection = () => {
     const [email, setEmail] = useState("")
     const [message, setMessage] = useState("")
 
+   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault(); // Evitar el comportamiento predeterminado del formulario
 
-    const checkedCompletFields = ()=>{
-        return firstName != "" && lastName != "" && email != "" && message != ""
-    }
+        // Aquí puedes realizar cualquier validación adicional que necesites antes de enviar el formulario
+
+        try {
+            setIsDisable(true)
+            const res = await fetch("/api/send", {
+                method: "POST",
+                body: JSON.stringify({
+                    email: email,
+                    firstname: firstName,
+                    lastname: lastName,
+                    message: message
+                })
+            });
+            setEmail("")
+            setLastName("")
+            setFirstName("")
+            setMessage("")
+            const data = await res.json();
+            console.log(data);
+        } catch (error) {
+            console.error("Error en la solicitud de red:", error);
+        }
+    };
+   
+
+    useEffect(()=>{
+        setIsDisable(firstName != "" && lastName != "" && email != "" && message != "")
+    }, [firstName, lastName, email, message])
+
 
     return ( <div className="bg-secondary dark:bg-accent-light my-[8rem] " id="contact">
         <ContainerCenter className="flex-col px-2 py-[3rem] ">
             <h3 className="font-bold text-[1.6rem] text-primary-dar dark:text-accent-text">Say hello</h3>
             <span className="text-[0.8rem] font-medium text-secondary-dark dark:text-accent-text mb-[3rem] ">Lorem Ipsum is simply dummy text of the printing .</span>
-            <form action="" className="w-full sm:w-2/3 flex flex-col gap-4">
+            <form onSubmit={handleSubmit} className="w-full sm:w-2/3 flex flex-col gap-4">
                 
 
                 <div className="flex w-full gap-4 flex-col xs:flex-row">
@@ -33,20 +61,8 @@ const ContactSection = () => {
                 <InputContact name="Email Address" id="iptEmailAddress" type="email" value={email} onValueChange={setEmail} />
                 <TextFieldContact name="Message" id="txtMessage" value={message} onValueChange={setMessage}/>
 
-                <ButtonPrimary className="w-full sm:self-end sm:w-fit px-[2rem]" onClick={async()=>{
-                    const res = await fetch("/api/send", {
-                        method: "POST",
-                        body:JSON.stringify({
-                            "email": email,
-                            "firstname": firstName,
-                            "lastname": lastName,
-                            "message": message
-                        })
-                    })
-                    const data = await res.json()
-                    console.log(data)
-                }}
-                    disable={!checkedCompletFields()}>
+                <ButtonPrimary className="w-full sm:self-end sm:w-fit px-[2rem]" 
+                    disable={!isDisable}>
                     Send
                 </ButtonPrimary>
             </form>
